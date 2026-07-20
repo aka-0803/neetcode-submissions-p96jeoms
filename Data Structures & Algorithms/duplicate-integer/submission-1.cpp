95a8c0e167fe71b9b43d3da0a7e8fa3978c299ad
AKASH  BAHADUR SINGH <92441172+aka-0803@users.noreply.github.com>
2026-07-20 23:29:38 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> hash;
+        for(int i=0;i<nums.size();i++){
+            if(hash.find(nums[i])!=hash.end()){
+                return true;
+            }
+            hash[nums[i]]++;
+        }
+
+        return false;
+    }
+};
